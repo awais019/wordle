3690664513cd335b69498a16ffd96e33f57dbbae
Muhammad Awais <muhammad.awais.20024@gmail.com>
2024-03-10 18:04:18 +0500

    Organize test suits into describe blocks

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -16,52 +16,63 @@ describe("WordleBoard", () => {
     await guessInput.trigger("keydown.enter");
   }
 
-  test("a victory message should appear when the user makes a guess that matches the word of the day.", async () => {
-    await playerSubmitsGuess(wordOfTheDay);
+  describe("End of the game messages", () => {
+    test("a victory message should appear when the user makes a guess that matches the word of the day.", async () => {
+      await playerSubmitsGuess(wordOfTheDay);
 
-    expect(wrapper.text()).toContain(VICTORY_MESSAGE);
+      expect(wrapper.text()).toContain(VICTORY_MESSAGE);
+    });
+
+    test("a defeat message appears if the user makes a guess that is incorrect.", async () => {
+      await playerSubmitsGuess("WRONG");
+
+      expect(wrapper.text()).toContain(DEFEAT_MESSAGE);
+    });
+
+    test("no end-of-game messge appears if the user has not yet made a guess.", async () => {
+      expect(wrapper.text()).not.toContain(VICTORY_MESSAGE);
+      expect(wrapper.text()).not.toContain(DEFEAT_MESSAGE);
+    });
   });
 
-  test("a defeat message appears if the user makes a guess that is incorrect.", async () => {
-    await playerSubmitsGuess("WRONG");
+  describe("rules for defining the word of the day", () => {
+    test("If a word of the day provided does not have exactly 5 characters, a warning message is emitted", async () => {
+      console.warn = vi.fn();
 
-    expect(wrapper.text()).toContain(DEFEAT_MESSAGE);
+      mount(WordleBoard, { props: { wordOfTheDay: "FLY" } });
+
+      expect(console.warn).toHaveBeenCalled();
+    });
+
+    test("If the word of the day is not all in uppercase, a warning is emitted", async () => {
+      console.warn = vi.fn();
+
+      mount(WordleBoard, { props: { wordOfTheDay: "tests" } });
+
+      expect(console.warn).toHaveBeenCalled();
+    });
+
+    test("If the word of the day is not a real word, a warning is emitted", async () => {
+      console.warn = vi.fn();
+
+      mount(WordleBoard, { props: { wordOfTheDay: "QWERT" } });
+
+      expect(console.warn).toHaveBeenCalled();
+    });
+
+    test("no warning is emitted if the word of the day is a real uppercase English word with 5 characters", async () => {
+      console.warn = vi.fn();
+
+      mount(WordleBoard, { props: { wordOfTheDay } });
+
+      expect(console.warn).not.toHaveBeenCalled();
+    });
   });
 
-  test("no end-of-game messge appears if the user has not yet made a guess.", async () => {
-    expect(wrapper.text()).not.toContain(VICTORY_MESSAGE);
-    expect(wrapper.text()).not.toContain(DEFEAT_MESSAGE);
-  });
-
-  test("If a word of the day provided does not have exactly 5 characters, a warning message is emitted", async () => {
-    console.warn = vi.fn();
-
-    mount(WordleBoard, { props: { wordOfTheDay: "FLY" } });
-
-    expect(console.warn).toHaveBeenCalled();
-  });
-
-  test("If the word of the day is not all in uppercase, a warning is emitted", async () => {
-    console.warn = vi.fn();
-
-    mount(WordleBoard, { props: { wordOfTheDay: "tests" } });
-
-    expect(console.warn).toHaveBeenCalled();
-  });
-
-  test("If the word of the day is not a real word, a warning is emitted", async () => {
-    console.warn = vi.fn();
-
-    mount(WordleBoard, { props: { wordOfTheDay: "QWERT" } });
-
-    expect(console.warn).toHaveBeenCalled();
-  });
-
-  test("no warning is emitted if the word of the day is a real uppercase English word with 5 characters", async () => {
-    console.warn = vi.fn();
-
-    mount(WordleBoard, { props: { wordOfTheDay } });
-
-    expect(console.warn).not.toHaveBeenCalled();
+  describe("player input", () => {
+    test.todo("player guesses are limited to 5 letters");
+    test.todo("player guesses are only submitted if they are real words");
+    test.todo("player guesses are not case sensitive");
+    test.todo("player guesses can only contain letters");
   });
 });
