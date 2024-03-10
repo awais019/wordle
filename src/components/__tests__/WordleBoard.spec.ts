b3f52a501c6c995a5e9e5ff637a61fa1b2976e69
Muhammad Awais <muhammad.awais.20024@gmail.com>
2024-03-10 18:31:18 +0500

    Move mock of console.warn to before each

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -36,6 +36,10 @@ describe("WordleBoard", () => {
   });
 
   describe("rules for defining the word of the day", () => {
+    beforeEach(() => {
+      console.warn = vi.fn();
+    });
+
     test.each([
       { wordOfTheDay: "FLY", reason: "word-of-the-day must have 5 characters" },
       {
@@ -49,8 +53,6 @@ describe("WordleBoard", () => {
     ])(
       "Since $reason: $wordOfTheDay is invalid, therefore a warning must be emitted",
       async ({ wordOfTheDay }) => {
-        console.warn = vi.fn();
-
         mount(WordleBoard, { props: { wordOfTheDay } });
 
         expect(console.warn).toHaveBeenCalled();
@@ -58,8 +60,6 @@ describe("WordleBoard", () => {
     );
 
     test("no warning is emitted if the word of the day is a real uppercase English word with 5 characters", async () => {
-      console.warn = vi.fn();
-
       mount(WordleBoard, { props: { wordOfTheDay } });
 
       expect(console.warn).not.toHaveBeenCalled();
