17beaec0f3839e1edc8fbb1acac928264355d4c8
Muhammad Awais <muhammad.awais.20024@gmail.com>
2024-03-10 16:22:15 +0500

    Only render end the game feedback after the game is over

--- src/components/WordleBoard.vue
+++ src/components/WordleBoard.vue
@@ -1,13 +1,23 @@
 <script setup lang="ts">
+  import { ref } from "vue";
   import { VICTORY_MESSAGE, DEFEAT_MESSAGE } from "@/settings";
 
   defineProps<{
     wordOfTheDay: string;
   }>();
+
+  const guessInProgress = ref("");
+  const guessSubmitted = ref("");
 </script>
 
 <template>
-  <input type="text" />
-  <p>{{ VICTORY_MESSAGE }}</p>
-  <p>{{ DEFEAT_MESSAGE }}</p>
+  <input
+    type="text"
+    v-model="guessInProgress"
+    @keydown.enter="guessSubmitted = guessInProgress"
+  />
+  <p
+    v-if="guessSubmitted.length > 0"
+    v-text="guessSubmitted == wordOfTheDay ? VICTORY_MESSAGE : DEFEAT_MESSAGE"
+  ></p>
 </template>
